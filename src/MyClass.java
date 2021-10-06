5207a0db98e444d230e38afad77181a6da2fdc2c
unknown <alfiancalonpresiden@gmail.com>
2021-10-06 09:40:16 +0700

    din umar

---
+++ src/MyClass.java
@@ -0,0 +1,11 @@
+/**
+ * App
+ */
+public class MyClass {
+
+    public static void main(String[] args) {
+        System.out.println("Nama   : Muhammad Umar Mansyur");
+        System.out.println("Alamat : Rombasan");
+        System.out.println("Kelas  : X ");
+    }
+}
